490d293393619a1afbbd5a08958d3cd246a1e3bd
SK MIRAJUL ISLAM <122096119+skmirajulislam@users.noreply.github.com>
2023-09-21 16:06:23 +0530

    commit by miraj

---
+++ JsHelp/Variable.js
@@ -0,0 +1,22 @@
+// js is case sensetive
+let a = 'Miraj' // blocks lavel scope its reliable not re declarable
+var b = 'Riji' // witin scope also redeclare
+const x = 3.14 // constant in js
+console.log(x)
+console.log(b)
+console.log(a)
+
+
+/* Example */
+let n = 3
+//let n ='Miraj' throw error
+n = 'hi'
+
+/* Example */
+var c = 'hi'
+var c = 3
+
+/* Example */
+const v = 3.14
+//v = 0 throw error
+//const v = 'X' throw error
